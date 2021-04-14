61645cfa66434e53b191bee104fa736b9f4d039e
tranphuquy19 <tranphuquy19@gmail.com>
2021-04-14 22:01:49 +0700

    update

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ let user = { name: 'nguyen b', age: 12 };
 user.address = "da nangbsauidhuia"
 delete user.age
 console.log(user)
+
+// let cong = function (a, b) {
+//     return a + b
+// }
+
+// function cong(a, b) {
+//     return a + b
+// }
+
+
+// let cong = (a, b) => {
+//     return a + b
+// }
+
+// let cong = (a, b) => a+b
+
+// sum = cong(1,2)
+
+// console.log(sum);
+
+// Các trường hợp ngoại lệ của khai bao biến
+
+a = 2; // chỗ nào trong chương trình cũng lấy đk giá trị biến a
+
+console.log(a)
